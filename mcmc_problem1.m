8731bb7a345eb760a29a4bf0409d4c1259273ca7
guanshixun <guanjie479@gmail.com>
2023-09-09 22:00:33 +0800

    use monte carlo method to do integrate and calculate the expectance

---
+++ mcmc_problem1.m
@@ -0,0 +1,13 @@
+%% I'll implement MCMC from scratch to strengthen the understanding.
+% Created on 2023.09.08
+% Problem1. Sample in a single parameter x and give the sampler as its
+% density function p(x) a Gaussian density with mean 2 and variance 2.
+%% parameters
+variance = 2;
+mean = 2;
+
+
+
+
+
+
